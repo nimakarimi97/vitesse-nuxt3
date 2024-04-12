6cc6d6f819432bbdf535c5b5647af1b47fda9fb1
Nima Karimi <nimakarimi676@gmail.com>
2024-04-12 10:55:37 +0200

    chore: added runtimeConfig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,20 @@
+import process from 'node:process'
 import { pwa } from './config/pwa'
 import { appDescription } from './constants/index'
 
 export default defineNuxtConfig({
+  runtimeConfig: {
+    // The private keys which are only available within server-side
+    apiSecret: '123', // can be overridden by NUXT_API_SECRET environment variable
+    EMAIL: process.env.EMAIL,
+    // Keys within public, will be also exposed to the client-side
+    DATABASE_ID: process.env.DATABASE_ID as string,
+    TASK_COLLECTION_ID: process.env.TASK_COLLECTION_ID as string,
+
+    public: {
+      apiBase: 'http://nimakarimi.com', // can be overridden by NUXT_PUBLIC_API_BASE environment variable
+    },
+  },
   modules: [
     '@vueuse/nuxt',
     '@unocss/nuxt',
