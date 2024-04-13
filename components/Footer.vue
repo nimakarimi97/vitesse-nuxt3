285d0e5a41e78af391fd3444b4ad371ead2de75b
Nima Karimi <nimakarimi676@gmail.com>
2024-04-13 20:26:44 +0200

    feat: updated the icons in the footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,10 +1,10 @@
 <template>
   <div text="xl gray4" m-5 flex="~ gap3" justify-center>
     <NuxtLink i-carbon-campsite to="/" />
-    <a i-carbon-logo-github href="https://github.com/antfu/vitesse-nuxt3" target="_blank" />
+    <a i-carbon-logo-github href="https://github.com/nimakarimi97/vitesse-nuxt3" target="_blank" />
     <DarkToggle />
 
-    <NuxtLink i-carbon-campsite to="/tasks" />
-    <NuxtLink i-carbon-campsite to="/login" />
+    <NuxtLink i-carbon:task to="/tasks" />
+    <NuxtLink i-carbon-login to="/login" />
   </div>
 </template>
